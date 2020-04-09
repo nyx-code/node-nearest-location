7f5c795503065ad8223ecc06deb9f2025a7f44c5
Shailesh Kadam <kadamshailesh007@gmail.com>
2020-04-10 01:19:30 +0530

    Removed CORS Options

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "*");
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "*");
+//     next();
+// });
 
 
 app.get('/', (req, res)=>{
